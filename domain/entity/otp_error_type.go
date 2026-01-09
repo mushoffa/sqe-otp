5f7256db3d0913cf5c7bd4ea762610f776447361
mushoffa <ridwan.mushoffa@gmail.com>
2026-01-09 11:20:13 +0700

    feat(otp): Update error type data structure

--- domain/entity/otp_error_type.go
+++ domain/entity/otp_error_type.go
@@ -1,9 +1,18 @@
 package entity
 
-import (
-	"errors"
+var (
+	ErrInvalidUserID = OtpError{Message: "invalid_user_id", Description: "User Id Cannot Empty"}
+	ErrOtpNotFound   = OtpError{Message: "otp_not_found", Description: "OTP Not Found"}
+	ErrOtpInvalid    = OtpError{Message: "otp_invalid", Description: "Invalid OTP"}
+	ErrOtpValidated  = OtpError{Message: "otp_validated", Description: "OTP is already validated"}
+	ErrOtpExpired    = OtpError{Message: "otp_expired", Description: "OTP is expired"}
 )
 
-var (
-	ErrInvalidUserID = errors.New("user_id cannot be empty string")
-)
+type OtpError struct {
+	Message     string `json:"error"`
+	Description string `json:"description"`
+}
+
+func (e OtpError) Error() string {
+	return e.Message
+}
