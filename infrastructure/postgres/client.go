8b3cd5df69d301237727b5b1b44cc492722fb1c7
mushoffa <ridwan.mushoffa@gmail.com>
2026-01-09 11:21:55 +0700

    feat(infrastructure): Add query & update record by condition function

--- infrastructure/postgres/client.go
+++ infrastructure/postgres/client.go
@@ -11,7 +11,8 @@ import (
 
 type DatabaseService interface {
 	Insert(context.Context, any) error
-	Query(context.Context, any) error
+	QueryByCondition(context.Context, map[string]any, any) error
+	UpdateByCondition(context.Context, map[string]any, any) error
 	Shutdown()
 }
 
@@ -39,8 +40,12 @@ func (c *client) Insert(ctx context.Context, table any) error {
 	return c.instance.WithContext(ctx).Create(table).Error
 }
 
-func (c *client) Query(ctx context.Context, table any) error {
-	return nil
+func (c *client) QueryByCondition(ctx context.Context, conditions map[string]any, table any) error {
+	return c.instance.WithContext(ctx).Where(conditions).Take(table).Error
+}
+
+func (c *client) UpdateByCondition(ctx context.Context, conditions map[string]any, table any) error {
+	return c.instance.WithContext(ctx).Where(conditions).Updates(table).Error
 }
 
 func (c *client) Shutdown() {
