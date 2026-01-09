719b9668e606ea7e85bb4948be391574ca501e9b
mushoffa <ridwan.mushoffa@gmail.com>
2026-01-09 11:23:42 +0700

    feat(infrastructure): Add BadRequest and InternalServerError helper function

--- infrastructure/rest/base_handler.go
+++ infrastructure/rest/base_handler.go
@@ -11,6 +11,10 @@ func (c *BaseHandler) Success(ctx *fiber.Ctx, data any) error {
 	return ctx.Status(fiber.StatusOK).JSON(data)
 }
 
+func (c *BaseHandler) BadRequest(ctx *fiber.Ctx, error any) error {
+	return ctx.Status(fiber.StatusBadRequest).JSON(error)
+}
+
 func (c *BaseHandler) InternalServerError(ctx *fiber.Ctx, error any) error {
 	return ctx.Status(fiber.StatusInternalServerError).JSON(error)
 }
