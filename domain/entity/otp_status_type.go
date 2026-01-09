4836d3ff2cc4977cb3f3c61684e3615fb6eb244e
mushoffa <ridwan.mushoffa@gmail.com>
2026-01-09 11:19:49 +0700

    feat(otp): Add driver function for database read/write

--- domain/entity/otp_status_type.go
+++ domain/entity/otp_status_type.go
@@ -1,5 +1,10 @@
 package entity
 
+import (
+	"database/sql/driver"
+	"fmt"
+)
+
 type OtpStatus int
 
 const (
@@ -10,4 +15,27 @@ const (
 
 func (e OtpStatus) String() string {
 	return [...]string{"created", "validated", "expired"}[e]
+}
+
+func (e *OtpStatus) Scan(value any) error {
+	status, ok := value.(string)
+	if !ok {
+		return fmt.Errorf("Cannot Scan Type %T OTP Status", value)
+	}
+
+	switch status {
+	case "created":
+		*e = OtpStatusCreated
+	case "validated":
+		*e = OtpStatusValidated
+	case "expired":
+		*e = OtpStatusExpired
+	default:
+		return fmt.Errorf("Unknown OTP Status Type %T", value)
+	}
+	return nil
+}
+
+func (e OtpStatus) Value() (driver.Value, error) {
+	return e.String(), nil
 }
